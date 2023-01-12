d07f48a80f192db36c195bfe5284af6cefe33b9e
Tyler Wysocki <115134475+TyWysocki@users.noreply.github.com>
2023-01-12 01:13:38 -0700

    Update helpers.h

--- filter-more/helpers.h
+++ filter-more/helpers.h
@@ -15,10 +15,6 @@ void edges(int height, int width, RGBTRIPLE image[height][width]);
 void blur(int height, int width, RGBTRIPLE image[height][width]);
 
 // NOTE: See how each funcction takes a 2D array called image as an argument.
-// image is an array of height specifying how many rows in the array
-// each row itself is another array of width many RGBTRIPLEs.
-// So if "image" represents the whole picture, then image[0] represents the first row
-// and image[0][0] represents the pixel in the upper-left corner of the image
 
 // Example: image[row][column] i.e. pixel in row number 1 and column number 1 is
 //          in the middle of the "block" of pixels below. image is just a 2D array
